852a488e8017e1b3d35116a36e5e26d330a4e3ad
Lavanya Kota <lavanya.kota@thinkitive.com>
2024-10-25 12:00:25 +0530

    lazy and eager loading implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { RegularComponent } from './regular/regular.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'regular',component:RegularComponent},
+   { path: 'lazy', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+ }

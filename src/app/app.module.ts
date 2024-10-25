b531308df6aef618cc4e7320ab2b8e43707445c2
Lavanya Kota <lavanya.kota@thinkitive.com>
2024-10-25 11:44:42 +0530

    implemented the concepts of lazy and eager loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegularComponent } from './regular/regular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegularComponent
   ],
   imports: [
     BrowserModule,
